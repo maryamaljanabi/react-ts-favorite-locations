686be8781bf4e088ab7646b469e9511aac12e813
maryamaljanabi <maryam.aljanabi135@gmail.com>
2022-09-01 15:21:51 +0300

    refactor: Change how Ids are names in local storage

--- src/helpers/addLocationToStorage.ts
+++ src/helpers/addLocationToStorage.ts
@@ -1,13 +1,24 @@
 export interface ILocation {
+  id?: string;
   name: string;
-  string: string;
+  street: string;
   postcode: string;
+  country?: string;
+  city?: string;
 }
 
+export interface IFavoriteLocations {
+  [id: string]: ILocation;
+}
+
+const getStructuredIdName = (id: string): string => {
+  return id.includes("locationId") ? id : `locationId:${id}`;
+};
+
 export const getFromLocalStorage = (id: string): ILocation | null => {
-  if (!localStorage.getItem(`locationId:${id}`)) return null;
+  if (!localStorage.getItem(getStructuredIdName(id))) return null;
   try {
-    return JSON.parse(localStorage.getItem(`locationId:${id}`) || "");
+    return JSON.parse(localStorage.getItem(getStructuredIdName(id)) || "");
   } catch (error) {
     console.log("error retrieving from local storage", error);
     return null;
@@ -15,13 +26,27 @@ export const getFromLocalStorage = (id: string): ILocation | null => {
 };
 
 export const addToLocalStorage = (id: string, value: ILocation): void => {
-  if (!getFromLocalStorage(`locationId:${id}`)) localStorage.setItem(`locationId:${id}`, JSON.stringify(value));
+  if (!getFromLocalStorage(getStructuredIdName(id))) localStorage.setItem(getStructuredIdName(id), JSON.stringify(value));
 };
 
 export const existsInLocalStorage = (id: string): boolean => {
-  return localStorage.getItem(`locationId:${id}`) ? true : false;
+  return localStorage.getItem(getStructuredIdName(id)) ? true : false;
 };
 
 export const removeFromLocalStorage = (id: string): void => {
-  if (localStorage.getItem(`locationId:${id}`)) localStorage.removeItem(`locationId:${id}`);
+  console.log("removing...", id);
+  if (localStorage.getItem(getStructuredIdName(id))) localStorage.removeItem(getStructuredIdName(id));
+};
+
+export const getAllLocalStorageItems = (): IFavoriteLocations => {
+  return Object.keys(localStorage).reduce<IFavoriteLocations>((acc, key) => {
+    if (key.includes("locationId")) {
+      try {
+        acc[key] = JSON.parse(localStorage[key]);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    return acc;
+  }, {});
 };
